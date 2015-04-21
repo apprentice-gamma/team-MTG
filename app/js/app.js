c76af9cb59fcdb349fe59e6bf50f96115e68fc66
mleah <mariah.hanson@detroitlabs.com>
2015-04-21 13:35:56 -0400

    starting to add routing

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,18 @@
 (function() {
-	var app = angular.module("app", ['ngRoute', 'ngResource', 'apikey']);
+	var app = angular.module("app", [
+		'ngRoute', 
+		'ngResource', 
+		'apikey'
+	]);
+
+	app.config(['$routeProvider', function($routeProvider) {
+		$routeProvider.
+			when('/', {
+				templateUrl: ''
+			})
+
+	}])
+
 
 	app.controller("testController", ["key", "$http", function(key, $http){
 		$http.jsonp("https://api.forecast.io/forecast/"+ key +"/42.332582,-83.045429?callback=JSON_CALLBACK").success(function(data){
