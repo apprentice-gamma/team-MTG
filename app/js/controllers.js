a306a618ee3647718b32482eba550a5673b72db1
mleah <mariah.hanson@detroitlabs.com>
2015-04-22 16:22:55 -0400

    adding in variables to the view

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,49 +1,44 @@
-var weatherControllers = angular.module('weatherControllers', []);
+(function() {
 
-weatherControllers.controller("FormController", ["GoogleGeo", "Forecast", "Geolocation", function(GoogleGeo, Forecast, Geolocation) {
-    var vm = this;
+    var weatherControllers = angular.module('weatherControllers', []);
 
-    Geolocation.get().then(function(coordinates) {
-        console.log(coordinates);
-    });
+    weatherControllers.controller("FormController", ["GoogleGeo", "Forecast", "Geolocation", function(GoogleGeo, Forecast, Geolocation) {
+        var vm = this;
 
-    vm.getCoordinates = function(address) {
-        GoogleGeo.get(address).then(function(coordinates) {
-            console.log(coordinates.lat);
-            console.log(coordinates.lng);
+        Geolocation.get().then(function(coordinates) {
+            console.log(coordinates);
+        });
+
+        vm.getCoordinates = function(address) {
+            GoogleGeo.get(address).then(function(coordinates) {
+                console.log(coordinates.lat);
+                console.log(coordinates.lng);
+                Forecast.get(coordinates.lat, coordinates.lng)
+                    .then(function(weather) {
+                        console.log(weather.currently);
+                    })
+            });
+        }
+    }]);
+
+    weatherControllers.controller("MainController", ["Forecast", "Geolocation", function(Forecast, Geolocation) {
+        var vm = this;
+        vm.temperature;
+        vm.feelsLike;
+        vm.humidity;
+        vm.windSpeed;
+
+        Geolocation.get().then(function(coordinates) {
             Forecast.get(coordinates.lat, coordinates.lng)
                 .then(function(weather) {
-                    console.log(weather.currently);
+                    vm.temperature = weather.currently.temperature;
+                    vm.feelsLike = weather.currently.apparentTemperature;
+                    vm.humidity = weather.currently.humidity * 100;
+                    vm.windSpeed = weather.currently.windSpeed;
                 })
         });
-    }
 
 
-}]);
+    }]);
 
-weatherControllers.controller("MainController", ["Forecast", "Geolocation", function(Forecast, Geolocation) {
-    var vm = this;
-    vm.temperature;
-
-    Geolocation.get().then(function(coordinates) {
-        console.log(coordinates);
-        Forecast.get(coordinates.lat, coordinates.lng)
-                .then(function(weather) {
-                    console.log(weather.currently);
-                    vm.temperature = weather.currently.temperature;
-                })
-    });
-
-
-}]);
-
-
-
-// (function() {
-//     var weatherControllers = angular.module('weatherControllers', []);
-
-//     weatherControllers.controller("FormController", ["GoogleGeo", "Forecast", "Geolocation", function(GoogleGeo, Forecast, Geolocation) {
-
-
-
-// })();
+})();
