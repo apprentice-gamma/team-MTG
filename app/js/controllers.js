23c288d2d4c87f191082f0075b2c390bb08fc624
mleah <mariah.hanson@detroitlabs.com>
2015-04-22 16:03:37 -0400

    WOO WE HAVE THE WEATHER!  mainController added and is working for the main template.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,22 +1,40 @@
 var weatherControllers = angular.module('weatherControllers', []);
 
 weatherControllers.controller("FormController", ["GoogleGeo", "Forecast", "Geolocation", function(GoogleGeo, Forecast, Geolocation) {
-  var vm = this;
+    var vm = this;
 
-  Geolocation.get().then(function(coordinates) {
-    console.log(coordinates);
-  });
+    Geolocation.get().then(function(coordinates) {
+        console.log(coordinates);
+    });
 
-  vm.getCoordinates = function(address) {
-    GoogleGeo.get(address).then(function(coordinates) {
-      console.log(coordinates.lat);
-      console.log(coordinates.lng);
-      Forecast.get(coordinates.lat, coordinates.lng)
-        .then(function(weather) {
-          console.log(weather.currently);
-        })
-   });
-  }
+    vm.getCoordinates = function(address) {
+        GoogleGeo.get(address).then(function(coordinates) {
+            console.log(coordinates.lat);
+            console.log(coordinates.lng);
+            Forecast.get(coordinates.lat, coordinates.lng)
+                .then(function(weather) {
+                    console.log(weather.currently);
+                })
+        });
+    }
+
+
+}]);
+
+weatherControllers.controller("MainController", ["Forecast", "Geolocation", function(Forecast, Geolocation) {
+    var vm = this;
+    vm.temperature;
+
+    Geolocation.get().then(function(coordinates) {
+        console.log(coordinates);
+        Forecast.get(coordinates.lat, coordinates.lng)
+                .then(function(weather) {
+                    console.log(weather.currently);
+                    vm.temperature = weather.currently.temperature;
+                })
+    });
+
+
 }]);
 
 
@@ -25,7 +43,7 @@ weatherControllers.controller("FormController", ["GoogleGeo", "Forecast", "Geolo
 //     var weatherControllers = angular.module('weatherControllers', []);
 
 //     weatherControllers.controller("FormController", ["GoogleGeo", "Forecast", "Geolocation", function(GoogleGeo, Forecast, Geolocation) {
-        
+
 
 
 // })();
