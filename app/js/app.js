220c155607bce45d641ce28483b49a5c10245ec6
Greg Santikian <gsantikian@gmail.com>
2015-04-24 11:54:54 -0400

    formatting and removal of unnecessary code

--- app/js/app.js
+++ app/js/app.js
@@ -1,40 +1,21 @@
 (function() {
-    var app = angular.module('app', [
-        'ngRoute',
-        'apikey',
-        'services',
-        'weatherControllers'
-    ]);
+	var app = angular.module('app', [
+		'ngRoute',
+		'apikey',
+		'services',
+		'weatherControllers'
+	]);
 
-    app.config(['$routeProvider', function($routeProvider) {
-        $routeProvider.
-        when('/', {
-            templateUrl: 'partials/main.html'
-        }).
-        when('/form', {
-            templateUrl: 'partials/form.html'
-        }).
-        when('/destination', {
-            templateUrl: 'partials/destination.html'
-        }).
-        when('/dash', {
-            templateUrl: 'dash.html'
-        });
-    }]);
-
-    // vm.open = openModal;
-
-    // function openModal(size) {
-    //     var modalInstance = $modal.open({
-    //         templateUrl: './partials/spinner_modal.html',
-    //         size: size,
-
-    //     });
-
-    //     modalInstance.result.then(function handleModal(selectedItem) {
-    //         $scope.selected = selectedItem;
-    //     }, function modalGone() {
-    //         $log.info('Modal dismissed at: ' + new Date());
-    //     });
-    // }
+	app.config(['$routeProvider', function($routeProvider) {
+		$routeProvider.
+		when('/', {
+			templateUrl: 'partials/main.html'
+		}).
+		when('/form', {
+			templateUrl: 'partials/form.html'
+		}).
+		when('/dash', {
+			templateUrl: 'dash.html'
+		});
+	}]);
 })();
--- app/js/services.js
+++ app/js/services.js
@@ -1,69 +1,69 @@
 var services = angular.module('services', []);
 
 services.factory('GoogleGeo', ['$q', function($q) {
-    return {
-        get: function(address) {
-            var geocoder = new google.maps.Geocoder();
-            var deferred = $q.defer();
-            geocoder.geocode({
-                address: address
-            }, function(results, status) {
-                var latLng = results[0].geometry.location;
-                var coordinates = {
-                    lat: String(latLng.k),
-                    lng: String(latLng.D)
-                };
-                return deferred.resolve(coordinates);
-            });
-            return deferred.promise;
-        }
-    };
+  return {
+    get: function(address) {
+      var geocoder = new google.maps.Geocoder();
+      var deferred = $q.defer();
+      geocoder.geocode({
+        address: address
+      }, function(results, status) {
+        var latLng = results[0].geometry.location;
+        var coordinates = {
+          lat: String(latLng.k),
+          lng: String(latLng.D)
+        };
+        return deferred.resolve(coordinates);
+      });
+      return deferred.promise;
+    }
+  };
 }]);
 
 services.factory('ReverseGeo', ['$q', function($q) {
-    return {
-        get: function(coordinates) {
-            var deferred = $q.defer();
-            var geocoder = new google.maps.Geocoder();
-            var latlng = new google.maps.LatLng(coordinates.lat, coordinates.lng);
-            geocoder.geocode({
-                'latLng': latlng
-            }, function(results, status) {
-                return deferred.resolve(results);
-            });
-            return deferred.promise;
-        }
-    };
+  return {
+    get: function(coordinates) {
+      var deferred = $q.defer();
+      var geocoder = new google.maps.Geocoder();
+      var latlng = new google.maps.LatLng(coordinates.lat, coordinates.lng);
+      geocoder.geocode({
+        'latLng': latlng
+      }, function(results, status) {
+        return deferred.resolve(results);
+      });
+      return deferred.promise;
+    }
+  };
 }]);
 
 services.factory('Forecast', ['$q', '$http', 'key', function($q, $http, key) {
-    return {
-        get: function(lat, lng) {
-            var deferred = $q.defer();
-            var rootUrl = 'https://api.forecast.io/forecast/';
-            $http.jsonp(rootUrl + key + '/' + lat + ',' + lng + '?callback=JSON_CALLBACK')
-                .success(function(weatherData) {
-                    return deferred.resolve(weatherData);
-                });
-            return deferred.promise;
-        }
-    };
+  return {
+    get: function(lat, lng) {
+      var deferred = $q.defer();
+      var rootUrl = 'https://api.forecast.io/forecast/';
+      $http.jsonp(rootUrl + key + '/' + lat + ',' + lng + '?callback=JSON_CALLBACK')
+        .success(function(weatherData) {
+          return deferred.resolve(weatherData);
+        });
+      return deferred.promise;
+    }
+  };
 }]);
 
 services.factory('Geolocation', ['$q', function($q) {
-    return {
-        get: function() {
-            var deferred = $q.defer();
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(function(position) {
-                    var coordinates = {
-                        lat: String(position.coords.latitude),
-                        lng: String(position.coords.longitude)
-                    };
-                    return deferred.resolve(coordinates);
-                });
-            }
-            return deferred.promise;
-        }
-    };
+  return {
+    get: function() {
+      var deferred = $q.defer();
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var coordinates = {
+            lat: String(position.coords.latitude),
+            lng: String(position.coords.longitude)
+          };
+          return deferred.resolve(coordinates);
+        });
+      }
+      return deferred.promise;
+    }
+  };
 }]);
